008c1c8f4577ca20aacef6578c8a9d9bdf92035f
Sri Sudersan <tgsuder@gmail.com>
2024-06-09 23:50:13 -0400

    Skeleton for Method Chaining

---
+++ MethodChaining.js
@@ -0,0 +1,5 @@
+// Explain method chaining in JavaScript by implementing a calculator that performs the basic actions like add, subtract, divide, and multiply.
+
+// calculator.add(10).subtract(2).divide(2).multiply(5);
+// console.log(calculator.total);
+// //20
