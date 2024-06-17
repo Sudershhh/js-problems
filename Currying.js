bb1488b5633c548a0f687f59ddeff5db88ae990d
Sri Sudersan <tgsuder@gmail.com>
2024-06-17 00:21:24 -0400

    Currying in JS

--- Currying.js
+++ Currying.js
@@ -4,22 +4,20 @@
 
 //all of these should return 6
 
-//OR
-
-//when we reach 5 arguments then return the value rather than new function
-// sum(1, 2, 3, 4, 5);
-// sum(1, 2)(3, 4, 5);
-// sum(1)(2, 3, 4, 5);
-// sum(1, 2, 3)(4, 5);
-// sum(1)(2)(3)(4)(5);
-// sum(1, 2, 3, 4)(5);
-
-//all should return 15
-
-function sum(...args) {
-  let value = 0;
-
-  return function (...args) {};
+function sum(initialSum = 0, ...args) {
+  return function (...argumentsArray) {
+    initialSum = args.reduce(
+      (acc, currentValue) => acc + currentValue,
+      initialSum
+    );
+    if (argumentsArray.length == 0) {
+      return initialSum;
+    } else {
+      return sum(initialSum, ...argumentsArray);
+    }
+  };
 }
 
-console.log(sum(1)(2)(3)());
+console.log(sum(1, 2)(3)());
+console.log(sum(1)(2, 3)());
+console.log(sum(1, 2, 3)());
